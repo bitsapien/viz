e1679ba22e69451612aba2146eaa60fc230f6cdf
C Rahul <crahul@elitmus.com>
2017-04-14 09:41:17 +0530

    added logo and utilising full screen

---
+++ js/generate.js
@@ -0,0 +1,501 @@
+    var users = [
+      {id: 1, name: "Ahmed, Zehra"},
+      {id: 2, name: "Beheshti, Babak"},
+      {id: 3, name: "Bloom, Nick"},
+      {id: 4, name: "Bravo, Amy"},
+      {id: 5, name: "Capozzi, Barbara"},
+      {id: 6, name: "Chowdhary, Sheetal"},
+      {id: 7, name: "Cinotti, Dan"},
+      {id: 8, name: "Cornelius, Matthew"},
+      {id: 9, name: "Donaldson, Elizabeth"},
+      {id: 10, name: "Dong, Ziqian (Cecilia)"},
+      {id: 11, name: "Fairbairn, Alan"},
+      {id: 12, name: "Fazal, Minaz"},
+      {id: 13, name: "Feldman, Rochelle"},
+      {id: 14, name: "Glazer, Fran"},
+      {id: 15, name: "Godsoe, Julie"},
+      {id: 16, name: "Grasso, Joanne"},
+      {id: 17, name: "Haar, Mindy"},
+      {id: 18, name: "Horton, Kevin"},
+      {id: 19, name: "Hsu, Hui-Yin"},
+      {id: 20, name: "Jeger, Abraham"},
+      {id: 21, name: "Jiang, Xueting"},
+      {id: 22, name: "Katz, Ellen"},
+      {id: 23, name: "Kirk, Colleen"},
+      {id: 24, name: "Krikelis, Lissi"},
+      {id: 25, name: "LaGrandeur, Kevin"},
+      {id: 26, name: "Li, Fang"},
+      {id: 27, name: "Luo, Laihan"},
+      {id: 28, name: "Martinez, Jim"},
+      {id: 29, name: "McNally, Adrienne"},
+      {id: 30, name: "Abramason, Tobi"}
+    ];
+    var likes = [
+{id: 1, name: 'Gerontology'},
+{id: 2, name: 'Mental Health and Aging'},
+{id: 3, name: 'Intergenerational Education'},
+{id: 4, name: 'Cultural Competency'},
+{id: 5, name: 'Global Health'},
+{id: 6, name: 'Community Health'},
+{id: 7, name: 'Wireless Technology'},
+{id: 8, name: 'Telecommunications'},
+{id: 9, name: 'Assistive Medical Devices'},
+{id: 10, name: 'Urban Administration'},
+{id: 11, name: 'Public Housing'},
+{id: 12, name: 'Environmentally Sustainable Design'},
+{id: 13, name: 'High Impact Teaching'},
+{id: 14, name: 'Service Learning'},
+{id: 15, name: 'Interprofessional Education'},
+{id: 16, name: 'Health Communications'},
+{id: 17, name: 'Art-Science Connections'},
+{id: 18, name: 'STEAM'},
+{id: 19, name: 'Biostatistics'},
+{id: 20, name: 'School Counseling'},
+{id: 21, name: 'Counseling Skills'},
+{id: 22, name: 'Professional Communication'},
+{id: 23, name: 'Leadership Training'},
+{id: 24, name: 'Motion Capture'},
+{id: 25, name: 'Virtual Reality'},
+{id: 26, name: 'Augmented Reality'},
+{id: 27, name: 'Mixed Reality'},
+{id: 28, name: 'Human Computer Interaction'},
+{id: 29, name: 'Robotics'},
+{id: 30, name: 'Medicine and Motion Study'},
+{id: 31, name: '3D Printing and Biomedicine'},
+{id: 32, name: 'Medicine and Literature'},
+{id: 33, name: 'Biomedical Ethics'},
+{id: 34, name: 'Medical Humanities'},
+{id: 35, name: 'Disability Studies'},
+{id: 36, name: 'American Literature'},
+{id: 37, name: 'Smart City'},
+{id: 38, name: 'Entrepreneurship'},
+{id: 39, name: 'Women in Science and Engineering'},
+{id: 40, name: 'Sustainable Tourism'},
+{id: 41, name: 'Data Analytics'},
+{id: 42, name: 'ePortfolios'},
+{id: 43, name: 'Online and Blended Learning'},
+{id: 44, name: 'Curriculum Design and Development'},
+{id: 45, name: 'Medical Ethics'},
+{id: 46, name: 'Narrative Medicine'},
+{id: 47, name: 'Faculty Learning Communities'},
+{id: 48, name: 'Publicity/Public Relations'},
+{id: 49, name: 'Editing'},
+{id: 50, name: 'Strategic Messaging '},
+{id: 51, name: 'American Studies'},
+{id: 52, name: 'Global Education'},
+{id: 53, name: 'Team Teaching'},
+{id: 54, name: 'Nutrition'},
+{id: 55, name: 'Health/Wellness Coaching'},
+{id: 56, name: 'Sports Medicine'},
+{id: 57, name: 'Community Gardening'},
+{id: 58, name: 'Journalism'},
+{id: 59, name: 'News Literacy'},
+{id: 60, name: 'Teacher Education'},
+{id: 61, name: 'STEM Education'},
+{id: 62, name: 'Medical Education'},
+{id: 63, name: 'Instructional Technology'},
+{id: 64, name: 'Information and Communication Technologies'},
+{id: 65, name: 'Gaming Literacy and Learning'},
+{id: 66, name: 'Clinical Empathy'},
+{id: 67, name: 'Psychiatry'},
+{id: 68, name: 'Psychology'},
+{id: 69, name: 'Psychology and Health'},
+{id: 70, name: 'Community Mental Health'},
+{id: 71, name: 'Organizational Leadership'},
+{id: 72, name: 'Conflict Management'},
+{id: 73, name: 'Migration Studies'},
+{id: 74, name: 'Emotional Self-Leadership'},
+{id: 75, name: 'Philosophy'},
+{id: 76, name: 'Ethics'},
+{id: 77, name: 'Medical Decision Making'},
+{id: 78, name: 'Consumer Emotions and Decision Making'},
+{id: 79, name: 'New Product Development and Marketing'},
+{id: 80, name: 'Technology and Marketing'},
+{id: 81, name: 'English as Second Language'},
+{id: 82, name: 'Children’s Literature'},
+{id: 83, name: 'Young Adult Literature'},
+{id: 84, name: 'Postmodern Metafiction'},
+{id: 85, name: 'Art-Science Connections'},
+{id: 86, name: 'Technology and Humanities'},
+{id: 87, name: 'Artificial Intelligence'},
+{id: 88, name: 'Robots'},
+{id: 89, name: 'Science and Technology Studies'},
+{id: 90, name: 'Future of Human Work'},
+{id: 91, name: 'Biomedical Engineering'},
+{id: 92, name: 'Micro/Nanotechnology'},
+{id: 93, name: 'Internet of Things'},
+{id: 94, name: 'Bioelectronics'},
+{id: 95, name: 'Math Education'}
+    ];
+
+
+    var user_likes = [
+{user_id: 1, like_id: 4},
+{user_id: 1, like_id: 5},
+{user_id: 1, like_id: 6},
+{user_id: 1, like_id: 3},
+{user_id: 2, like_id: 7},
+{user_id: 2, like_id: 8},
+{user_id: 2, like_id: 9},
+{user_id: 3, like_id: 10},
+{user_id: 3, like_id: 11},
+{user_id: 3, like_id: 12},
+{user_id: 4, like_id: 13},
+{user_id: 4, like_id: 14},
+{user_id: 5, like_id: 15},
+{user_id: 5, like_id: 5},
+{user_id: 5, like_id: 16},
+{user_id: 6, like_id: 13},
+{user_id: 6, like_id: 17},
+{user_id: 6, like_id: 18},
+{user_id: 6, like_id: 19},
+{user_id: 6, like_id: 5},
+{user_id: 7, like_id: 20},
+{user_id: 7, like_id: 21},
+{user_id: 7, like_id: 22},
+{user_id: 7, like_id: 23},
+{user_id: 8, like_id: 24},
+{user_id: 8, like_id: 25},
+{user_id: 8, like_id: 26},
+{user_id: 8, like_id: 27},
+{user_id: 8, like_id: 28},
+{user_id: 8, like_id: 29},
+{user_id: 8, like_id: 30},
+{user_id: 8, like_id: 31},
+{user_id: 9, like_id: 32},
+{user_id: 9, like_id: 33},
+{user_id: 9, like_id: 34},
+{user_id: 9, like_id: 35},
+{user_id: 9, like_id: 36},
+{user_id: 10, like_id: 26},
+{user_id: 10, like_id: 9},
+{user_id: 10, like_id: 12},
+{user_id: 10, like_id: 37},
+{user_id: 10, like_id: 38},
+{user_id: 10, like_id: 39},
+{user_id: 11, like_id: 40},
+{user_id: 11, like_id: 12},
+{user_id: 12, like_id: 23},
+{user_id: 12, like_id: 41},
+{user_id: 12, like_id: 38},
+{user_id: 12, like_id: 42},
+{user_id: 12, like_id: 43},
+{user_id: 12, like_id: 44},
+{user_id: 13, like_id: 32},
+{user_id: 13, like_id: 45},
+{user_id: 13, like_id: 46},
+{user_id: 14, like_id: 47},
+{user_id: 14, like_id: 13},
+{user_id: 14, like_id: 44},
+{user_id: 14, like_id: 43},
+{user_id: 15, like_id: 48},
+{user_id: 15, like_id: 49},
+{user_id: 15, like_id: 50},
+{user_id: 16, like_id: 51},
+{user_id: 16, like_id: 52},
+{user_id: 16, like_id: 53},
+{user_id: 17, like_id: 54},
+{user_id: 17, like_id: 1},
+{user_id: 17, like_id: 55},
+{user_id: 17, like_id: 56},
+{user_id: 17, like_id: 6},
+{user_id: 17, like_id: 57},
+{user_id: 18, like_id: 58},
+{user_id: 18, like_id: 59},
+{user_id: 18, like_id: 48},
+{user_id: 18, like_id: 49},
+{user_id: 19, like_id: 60},
+{user_id: 19, like_id: 61},
+{user_id: 19, like_id: 62},
+{user_id: 19, like_id: 63},
+{user_id: 19, like_id: 64},
+{user_id: 19, like_id: 44},
+{user_id: 19, like_id: 43},
+{user_id: 19, like_id: 65},
+{user_id: 20, like_id: 66},
+{user_id: 20, like_id: 16},
+{user_id: 20, like_id: 67},
+{user_id: 20, like_id: 68},
+{user_id: 20, like_id: 69},
+{user_id: 20, like_id: 70},
+{user_id: 21, like_id: 71},
+{user_id: 21, like_id: 23},
+{user_id: 21, like_id: 72},
+{user_id: 21, like_id: 73},
+{user_id: 21, like_id: 74},
+{user_id: 22, like_id: 75},
+{user_id: 22, like_id: 76},
+{user_id: 23, like_id: 77},
+{user_id: 23, like_id: 78},
+{user_id: 23, like_id: 79},
+{user_id: 23, like_id: 80},
+{user_id: 24, like_id: 81},
+{user_id: 24, like_id: 22},
+{user_id: 24, like_id: 83},
+{user_id: 24, like_id: 43},
+{user_id: 24, like_id: 84},
+{user_id: 25, like_id: 17},
+{user_id: 25, like_id: 43},
+{user_id: 25, like_id: 86},
+{user_id: 25, like_id: 18},
+{user_id: 25, like_id: 76},
+{user_id: 25, like_id: 87},
+{user_id: 25, like_id: 88},
+{user_id: 25, like_id: 89},
+{user_id: 25, like_id: 90},
+{user_id: 25, like_id: 33},
+{user_id: 26, like_id: 91},
+{user_id: 26, like_id: 92},
+{user_id: 26, like_id: 37},
+{user_id: 26, like_id: 93},
+{user_id: 26, like_id: 94},
+{user_id: 27, like_id: 95},
+{user_id: 27, like_id: 27},
+{user_id: 28, like_id: 37},
+{user_id: 28, like_id: 13},
+{user_id: 28, like_id: 14},
+{user_id: 28, like_id: 63},
+{user_id: 28, like_id: 61},
+{user_id: 29, like_id: 14},
+{user_id: 30, like_id: 1},
+{user_id: 30, like_id: 2},
+{user_id: 30, like_id: 3}
+    ];
+    var groups = [
+{id: 1, name: 'Interdisciplinary Health Sciences'},
+{id: 2, name: 'Physician Assistant Studies'},
+{id: 3, name: 'Electrical and Computer Engineering'},
+{id: 4, name: 'Social Sciences'},
+{id: 5, name: 'International and Experiential Education'},
+{id: 6, name: 'Osteopathic Medicine'},
+{id: 7, name: 'Interdisciplinary Studies'},
+{id: 8, name: 'Education'},
+{id: 9, name: 'Digital Art and Design'},
+{id: 10, name: 'English'},
+{id: 11, name: 'Hospitality Management'},
+{id: 12, name: 'Center for Teaching and Learning'},
+{id: 13, name: 'Communications and Marketing'},
+{id: 14, name: 'Communication Arts'},
+{id: 15, name: 'Management and Marketing'},
+{id: 16, name: 'Social Science'},
+{id: 17, name: 'Mechanical Engineering'},
+{id: 18, name: 'Math'}
+    ];
+    var user_groups = [
+      {user_id: 1, group_id: 2},
+      {user_id: 2, group_id: 3},
+      {user_id: 3, group_id: 4},
+      {user_id: 4, group_id: 5},
+      {user_id: 5, group_id: 6},
+      {user_id: 6, group_id: 7},
+      {user_id: 7, group_id: 8},
+      {user_id: 8, group_id: 9},
+      {user_id: 9, group_id: 10},
+      {user_id: 10, group_id: 3},
+      {user_id: 11, group_id: 11},
+      {user_id: 12, group_id: 8},
+      {user_id: 13, group_id: 10},
+      {user_id: 14, group_id: 12},
+      {user_id: 15, group_id: 13},
+      {user_id: 16, group_id: 4},
+      {user_id: 17, group_id: 1},
+      {user_id: 18, group_id: 14},
+      {user_id: 19, group_id: 8},
+      {user_id: 20, group_id: 6},
+      {user_id: 21, group_id: 15},
+      {user_id: 22, group_id: 16},
+      {user_id: 23, group_id: 15},
+      {user_id: 24, group_id: 10},
+      {user_id: 25, group_id: 10},
+      {user_id: 26, group_id: 17},
+      {user_id: 27, group_id: 18},
+      {user_id: 28, group_id: 8},
+      {user_id: 29, group_id: 5},
+      {user_id: 30, group_id: 1}
+    ];
+    function unique(value, index, self) { 
+      return self.indexOf(value) === index;
+  }
+    function get_likes(group_id) {
+      filtered_group = user_groups.filter(function(v) {
+          return v.group_id == group_id
+      });
+      like_ids = [];
+      for(var i = 0;i < filtered_group.length; i++) {
+        user_like = user_likes.filter(function(v){
+          return v.user_id == filtered_group[i].user_id
+        });
+        for(var j = 0; j< user_like.length; j++) {
+          like_ids.push(user_like[j].like_id)
+        }
+      }
+      return like_ids.filter(unique);
+    }
+    var connection = function(from, to){
+        from_likes = get_likes(from);
+        to_likes = get_likes(to);
+        count = $.grep(from_likes, function(element) {
+        return $.inArray(element, to_likes ) !== -1;
+    }).length;
+    return {from: from, to: to, value: count, title: count+' common likes'};
+    console.log('========='+from+' to '+to+'=========')
+    }
+  var combine = function(a, min) {
+      var fn = function(n, src, got, all) {
+          if (n == 0) {
+              if (got.length > 0) {
+                  all[all.length] = got;
+              }
+              return;
+          }
+          for (var j = 0; j < src.length; j++) {
+              fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+          }
+          return;
+      }
+      var all = [];
+      for (var i = min; i < a.length; i++) {
+          fn(i, a, [], all);
+      }
+      all.push(a);
+      return all;
+  }
+
+    var nodes = null;
+    var edges = null;
+    var network = null;
+
+    function draw_groups(ele) {
+      $('#graph_options a').removeClass('active');
+      $(ele).addClass('active');
+      // create people.
+      // id : 1,2,3
+      // value: number of people
+      // label: names of groups
+      nodes = [];
+      for(var i = 0; i < groups.length; i++) {
+        count = user_groups.filter(function( v) {
+          return v.group_id == groups[i].id
+        }).length;
+        group = {id: groups[i].id, value: count, label: groups[i].name}
+        nodes.push(group);
+      }
+
+      // nodes = [
+      //   {id: 1,  value: 2,  label: 'Administrators' },
+      //   {id: 2,  value: 31, label: 'Faculty'},
+      //   {id: 3,  value: 12, label: 'Students'}
+      // ];
+      // nodes = [
+      //   {id: 1,  value: 2,  label: 'Algie' },
+      //   {id: 2,  value: 31, label: 'Alston'},
+      //   {id: 3,  value: 12, label: 'Barney'},
+      //   {id: 4,  value: 16, label: 'Coley' },
+      //   {id: 5,  value: 17, label: 'Grant' },
+      //   {id: 6,  value: 15, label: 'Langdon'},
+      //   {id: 7,  value: 6,  label: 'Lee'},
+      //   {id: 8,  value: 5,  label: 'Merlin'},
+      //   {id: 9,  value: 30, label: 'Mick'},
+      //   {id: 10, value: 18, label: 'Tod'},
+      // ];
+
+      // create connections between people
+      // from: one group id
+      // to: another group id
+      // value: number of similar likes
+      edges = [];
+      permuted_groups = groups;
+      for(var i = 1; i < permuted_groups.length; i++) {
+        from = permuted_groups[i-1].id;
+        to = permuted_groups[i].id;
+        c = connection(from, to)
+        if(c.value != 0) {
+          edges.push(c);
+        }
+      }
+      // edges = [
+      //   {from: 2, to: 8, value: 3, title: '3 emails per week'},
+      //   {from: 2, to: 9, value: 5, title: '5 emails per week'},
+      //   {from: 2, to: 10,value: 1, title: '1 emails per week'},
+      //   {from: 4, to: 6, value: 8, title: '8 emails per week'},
+      //   {from: 5, to: 7, value: 2, title: '2 emails per week'},
+      //   {from: 4, to: 5, value: 1, title: '1 emails per week'},
+      //   {from: 9, to: 10,value: 2, title: '2 emails per week'},
+      //   {from: 2, to: 3, value: 6, title: '6 emails per week'},
+      //   {from: 3, to: 9, value: 4, title: '4 emails per week'},
+      //   {from: 5, to: 3, value: 1, title: '1 emails per week'},
+      //   {from: 2, to: 7, value: 4, title: '4 emails per week'}
+      // ];
+
+      // Instantiate our network object.
+      var container = document.getElementById('group_likes');
+      var data = {
+        nodes: nodes,
+        edges: edges
+      };
+      var options = {
+        nodes: {
+          shape: 'dot',
+          scaling:{
+            label: {
+              min:8,
+              max:20
+            }
+          }
+        }
+      };
+      network = new vis.Network(container, data, options);
+    }
+    $(document).ready(function(){
+        $('.dataset').hide();
+        for(var i = 0; i < users.length; i++){
+          likes_of_user = user_likes.filter(function(v){
+            return v.user_id == users[i].id
+          });
+          likes_ids = [];
+          for(var j = 0; j < likes_of_user.length; j++) {
+            likes_ids.push(likes_of_user[j].like_id);
+          }
+          likes_names = [];
+          for(var k = 0; k < likes_ids.length; k++) {
+            like = likes.filter(function(l){
+              return l.id == likes_ids[k] 
+            })[0];
+            likes_names.push(like.name);
+          }
+          
+          $('#users table tbody').append('<tr><td>'+users[i].id+'</td><td>'+users[i].name+'</td><td>'+likes_names+'</td></tr>');
+        }      
+        for(var i = 0; i < groups.length; i++){
+          groups_of_user = user_groups.filter(function(v){
+            return v.group_id == groups[i].id
+          });
+          user_ids = [];
+          // console.log(groups_of_user)
+          for(var j = 0; j < groups_of_user.length; j++) {
+            user_ids.push(groups_of_user[j].user_id);
+          }
+          user_names = [];
+          for(var k = 0; k < user_ids.length; k++) {
+            user = users.filter(function(u){
+              return u.id == user_ids[k] 
+            })[0];
+            user_names.push(user.name);
+          }          
+          $('#groups table tbody').append('<tr><td>'+groups[i].id+'</td><td>'+groups[i].name+'</td><td>'+user_names+'</td></tr>');
+        }      
+        for(var i = 0; i < likes.length; i++){
+          $('#likes table tbody').append('<tr><td>'+likes[i].id+'</td><td>'+likes[i].name+'</td></tr>');
+        }      
+    });
+    function showData(dataset) {
+      $('#options a').removeClass('active');
+      $('#'+dataset+'-data-btn').addClass('active');
+      $('.dataset').hide();
+      $('#'+dataset).show();
+    }
+
+    function init(){
+      $('#graph_options a')[0].click();
+      $('#options a')[1].click();
+    }
